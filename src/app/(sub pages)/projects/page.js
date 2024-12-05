73d0896f062b9d5afb1c27ea13b93b6196e60cb5
slocksert <rsdlj01@gmail.com>
2024-12-05 02:54:33 -0300

    Replace Dice model with Killua and restructure layout for improved project display

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -5,7 +5,7 @@ import { projectsData } from "../../data";
 import RenderModel from "@/components/RenderModel";
 import dynamic from "next/dynamic";
 
-const Dice = dynamic(() => import("@/components/models/Dice"), {
+const Killua = dynamic(() => import("@/components/models/Killua"), {
   ssr: false,
 });
 
@@ -15,22 +15,28 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <>
+    <main className="fixed inset-0 overflow-hidden">
       <Image
         src={bg}
         alt="Next.js Portfolio website's about page background image"
-        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
+        className="absolute inset-0 w-full h-full object-cover object-center opacity-50"
         priority
         sizes="100vw"
       />
 
-      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0 z-10">
-        <RenderModel>
-          <Dice />
-        </RenderModel>
+      <div className="absolute inset-0">
+        <div className="w-full h-full">
+          <div className="absolute inset-0 flex items-center justify-center">
+            <ProjectList projects={projectsData} />
+          </div>
+          
+          <div className="absolute inset-0">
+            <RenderModel>
+              <Killua />
+            </RenderModel>
+          </div>
+        </div>
       </div>
-
-      <ProjectList projects={projectsData} />
-    </>
+    </main>
   );
 }
