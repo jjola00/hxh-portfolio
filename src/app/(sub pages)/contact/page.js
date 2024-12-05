54a5d6a57978d8edc840aa48eeb5e773d3bd50af
slocksert <rsdlj01@gmail.com>
2024-12-05 19:41:18 -0300

    Add dynamic 3D model rendering to Contact page

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png";
+import RenderModel from "@/components/RenderModel";
 import Form from "@/components/contact/Form";
+import dynamic from "next/dynamic";
+const Computer = dynamic(() => import("@/components/models/Computer"), {
+  ssr: false,
+});
 
 export const metadata = {
   title: "Contact",
@@ -16,6 +21,12 @@ export default function Contact() {
         sizes="100vw"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
       />
+
+      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen sm:top-1/2 top-1/4 absolute -translate-y-1/2 left-0 z-10">
+        <RenderModel>
+          <Computer />
+        </RenderModel>
+      </div>
 
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
