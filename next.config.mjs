8a1bf4e4ffdeeaf9ef9c8f43e057122beaf561f7
slocksert <rsdlj01@gmail.com>
2024-12-05 15:49:25 -0300

    Configure Next.js to allow SVG images and define remote patterns for external image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'github-readme-stats.vercel.app'
+            },
+            {
+                protocol: 'https',
+                hostname: 'skillicons.dev'
+            },
+            {
+                protocol: 'https',
+                hostname: 'github-readme-streak-stats.herokuapp.com'
+            }
+        ]
+    },
+  }
+  
+export default nextConfig
