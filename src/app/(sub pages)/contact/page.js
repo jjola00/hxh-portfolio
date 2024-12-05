ca92aa2c252b2b50d500171971941e909bbc71a1
slocksert <rsdlj01@gmail.com>
2024-12-05 18:34:58 -0300

    Update Contact page title and description for clarity and professionalism

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -20,13 +20,10 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-blue-200 font-semibold text-center text-4xl capitalize">
-          Summon the Nen user
+          Contact Me
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
-          Step into the world of Nen and send your messages through the digital realm.
-          Whether you&apos;re seeking alliances, solving puzzles, or sharing your journey,
-          your words take form in this vast network. Use the interface below to transmit 
-          your thoughts through the virtual space, and await a response powered by Nen.
+            If you have any questions or would like to discuss a project, feel free to reach out.
           </p>
         </div>
         <Form />
